b4ca42697d74e337c671ebabd321217304e1b974
Hayden Heroux <haydenheroux@gmail.com>
2024-01-14 13:28:14 -0500

    chore: Format.

--- src/main/java/frc/lib/Telemetry.java
+++ src/main/java/frc/lib/Telemetry.java
@@ -73,23 +73,26 @@ public class Telemetry {
 
   /**
    * Adds swerve module states to a Shuffleboard tab.
-   * 
+   *
    * @param tab the Shuffleboard tab to add the swerve module states to.
    * @param title the title of the swerve module states.
    * @param swerveModuleStatesSupplier a supplier for swerve module states.
    */
-  public static void addSwerveModuleStates(ShuffleboardTab tab, String title, Supplier<SwerveModuleState[]> swerveModuleStatesSupplier) {
-    tab.addDoubleArray(title, () -> {
-      SwerveModuleState[] states = swerveModuleStatesSupplier.get();
-      double[] doubles = new double[8];
+  public static void addSwerveModuleStates(
+      ShuffleboardTab tab, String title, Supplier<SwerveModuleState[]> swerveModuleStatesSupplier) {
+    tab.addDoubleArray(
+        title,
+        () -> {
+          SwerveModuleState[] states = swerveModuleStatesSupplier.get();
+          double[] doubles = new double[8];
 
-      for (int i = 0; i < 4; i++) {
-        SwerveModuleState state = states[i];
-        doubles[2 * i] = state.angle.getDegrees();
-        doubles[2 * i + 1] = state.speedMetersPerSecond;
-      }
+          for (int i = 0; i < 4; i++) {
+            SwerveModuleState state = states[i];
+            doubles[2 * i] = state.angle.getDegrees();
+            doubles[2 * i + 1] = state.speedMetersPerSecond;
+          }
 
-      return doubles;
-    });
+          return doubles;
+        });
   }
 }
