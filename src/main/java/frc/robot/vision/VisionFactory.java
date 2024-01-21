56551f4d5e2d2d342ba998c768d3928615b37450
Hayden Heroux <haydenheroux@gmail.com>
2024-01-20 20:17:57 -0500

    feat(vision): Add simulated vision pose estimator.

--- src/main/java/frc/robot/vision/VisionFactory.java
+++ src/main/java/frc/robot/vision/VisionFactory.java
@@ -2,18 +2,17 @@ package frc.robot.vision;
 
 import frc.robot.Robot;
 
-/** Helper class for creating hardware for the vision subsystem.  */
+/** Helper class for creating hardware for the vision subsystem. */
 public class VisionFactory {
-   
-    /**
-     * Creates a vision pose estimator.
-     * 
-     * @return a vision pose estimator.
-     */
-    public static VisionPoseEstimatorIO createVisionPoseEstimator() {
-        if (Robot.isReal()) return new VisionPoseEstimatorIOSim(); // TODO
 
-        return new VisionPoseEstimatorIOSim();
-    }
+  /**
+   * Creates a vision pose estimator.
+   *
+   * @return a vision pose estimator.
+   */
+  public static VisionPoseEstimatorIO createVisionPoseEstimator() {
+    if (Robot.isReal()) return new VisionPoseEstimatorIOSim(); // TODO
 
+    return new VisionPoseEstimatorIOSim();
+  }
 }
--- src/main/java/frc/robot/vision/VisionPoseEstimatorIO.java
+++ src/main/java/frc/robot/vision/VisionPoseEstimatorIO.java
@@ -1,27 +1,26 @@
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
-
+import edu.wpi.first.math.geometry.Pose3d;
 import java.util.Optional;
 
 /** Vision pose estimator hardware interface. */
 public interface VisionPoseEstimatorIO {
 
-    /** Configures the vision pose estimator. */
-    public void configure();
+  /** Configures the vision pose estimator. */
+  public void configure();
 
-    /**
-     * Sets the vision pose estimator's reference position.
-     * 
-     * @param referencePosition the reference position.
-     */
-    public void setReferencePosition(Pose2d referencePosition);
+  /**
+   * Sets the vision pose estimator's reference position.
+   *
+   * @param referencePosition the reference position.
+   */
+  public void setReferencePosition(Pose2d referencePosition);
 
-    /**
-     * Gets the estimated position.
-     * 
-     * @return the estiamted position.
-     */
-    public Optional getPosition();
-    
+  /**
+   * Gets the estimated position.
+   *
+   * @return the estimated position.
+   */
+  public Optional<Pose3d> getPosition();
 }
