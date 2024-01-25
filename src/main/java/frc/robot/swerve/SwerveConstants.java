4942fd56576ecf5ba54eb537e45066a0a6128031
Hayden Heroux <haydenheroux@gmail.com>
2024-01-25 12:41:02 -0500

    refactor(swerve): Change visibility of acceleration calculation.

--- src/main/java/frc/robot/swerve/SwerveConstants.java
+++ src/main/java/frc/robot/swerve/SwerveConstants.java
@@ -98,11 +98,11 @@ public class SwerveConstants {
    * Calculates an acceleration using a ramp duration.
    *
    * @param maximumSpeed the maximum speed in units per second.
-   * @param rampDurationSeconds the desired duration to ramp from no speed to full speed.
+   * @param desiredRampDurationSeconds the desired duration to ramp from no speed to full speed.
    * @return the acceleration in units per second per second.
    */
-  public static double calculateAcceleration(double maximumSpeed, double rampDurationSeconds) {
-    return maximumSpeed / rampDurationSeconds;
+  private static double calculateAcceleration(double maximumSpeed, double desiredRampDurationSeconds) {
+    return maximumSpeed / desiredRampDurationSeconds;
   }
 
   /** Maximum acceleration in meters per second per second. */
