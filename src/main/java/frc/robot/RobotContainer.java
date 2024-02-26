9fd970c74cca79e588c756b2a212f1140efde807
Hayden Heroux <haydenheroux@gmail.com>
2024-02-26 05:06:41 -0500

    feat(auto): Add multi-subsystem commands.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,15 +2,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib.Telemetry;
 import frc.robot.arm.Arm;
-import frc.robot.arm.ArmState;
 import frc.robot.auto.Auto;
 import frc.robot.climber.Climber;
 import frc.robot.intake.Intake;
-import frc.robot.intake.IntakeConstants.PivotMotorConstants;
 import frc.robot.lights.Lights;
 import frc.robot.odometry.Odometry;
 import frc.robot.shooter.Shooter;
@@ -71,26 +68,12 @@ public class RobotContainer {
     driverController.y().onTrue(odometry.tare());
     driverController.x().whileTrue(swerve.cross());
 
-    Command toIntake =
-        Commands.parallel(
-            Commands.waitUntil(intake::isNotStowed).andThen(arm.to(ArmState.INTAKE)), intake.out());
-
-    Command toStow =
-        Commands.runOnce(
-            () -> {
-              arm.setGoal(ArmState.STOW);
-              intake.setPivotGoal(PivotMotorConstants.MAXIMUM_ANGLE);
-            });
-
     operatorController
         .leftTrigger()
-        .whileTrue(toIntake.andThen(Commands.parallel(intake.intake(), shooter.intake())))
-        .onFalse(toStow);
+        .whileTrue(auto.readyIntake().andThen(auto.intakeNote()))
+        .onFalse(auto.stow());
 
-    operatorController
-        .rightTrigger()
-        .whileTrue(arm.to(ArmState.SHOOT).andThen(shooter.shoot()))
-        .onFalse(Commands.runOnce(() -> arm.setGoal(ArmState.STOW)));
+    operatorController.rightTrigger().whileTrue(auto.shoot()).onFalse(auto.stow());
   }
 
   /**
