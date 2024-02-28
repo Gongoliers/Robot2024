9fa22993d4633c1648ee9d52f62955fa7b6be0ef
Hayden Heroux <haydenheroux@gmail.com>
2024-02-28 18:38:40 -0500

    feat(shooter): Use voltage output request.

--- src/main/java/frc/robot/shooter/FlywheelMotorIOTalonFX.java
+++ src/main/java/frc/robot/shooter/FlywheelMotorIOTalonFX.java
@@ -1,5 +1,6 @@
 package frc.robot.shooter;
 
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
@@ -7,10 +8,14 @@ public class FlywheelMotorIOTalonFX implements FlywheelMotorIO {
 
     private final TalonFX talonFX;
 
+    private final VoltageOut voltageOutRequest;
+
     public FlywheelMotorIOTalonFX() {
         int canId = 39;
 
         talonFX = new TalonFX(canId);
+
+        voltageOutRequest = new VoltageOut(0);
     }
 
 
@@ -29,7 +34,7 @@ public class FlywheelMotorIOTalonFX implements FlywheelMotorIO {
 
     @Override
     public void setVoltage(double volts) {
-        talonFX.setVoltage(volts);
+        talonFX.setControl(voltageOutRequest.withOutput(volts));
     }
 
     @Override
