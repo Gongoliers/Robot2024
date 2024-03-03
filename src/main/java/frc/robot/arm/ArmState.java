8a2c16d1af7d84fcf02635bedaef6ab02d5022aa
Hayden Heroux <haydenheroux@gmail.com>
2024-03-03 15:26:37 -0500

    new wrist intake angle

--- src/main/java/frc/robot/arm/ArmState.java
+++ src/main/java/frc/robot/arm/ArmState.java
@@ -17,9 +17,9 @@ public record ArmState(State shoulder, State wrist) {
   public static final ArmState STOW =
       new ArmState(ShoulderMotorConstants.MINIMUM_ANGLE, WristMotorConstants.MAXIMUM_ANGLE);
 
-  public static final ArmState SHOOT = STOW.withWrist(Rotation2d.fromDegrees(23.265));
+  public static final ArmState SHOOT = STOW.withWrist(Rotation2d.fromDegrees(18));
 
-  public static final ArmState INTAKE = STOW.withWrist(Rotation2d.fromDegrees(0));
+  public static final ArmState INTAKE = STOW.withWrist(Rotation2d.fromDegrees(4));
 
   public static final ArmState AMP =
       new ArmState(ShoulderMotorConstants.MAXIMUM_ANGLE, Rotation2d.fromDegrees(0));
