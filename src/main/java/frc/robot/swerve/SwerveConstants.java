d04ec445891e24737e73f2474968bbdecf4ea74f
Hayden Heroux <haydenheroux@gmail.com>
2024-03-14 20:26:55 -0400

    fix: Working swerve.

--- src/main/java/frc/robot/swerve/SwerveConstants.java
+++ src/main/java/frc/robot/swerve/SwerveConstants.java
@@ -67,28 +67,28 @@ public class SwerveConstants {
       new SwerveModuleConfig(
           new SwerveModuleCAN(11, 10, 12, SWERVE_BUS),
           new Translation2d(X_OFFSET, Y_OFFSET),
-          Rotation2d.fromRotations(-0.027100));
+          Rotation2d.fromRotations(.117188));
 
   /** Module configuration for the north east swerve module. */
   public static final SwerveModuleConfig NORTH_EAST_MODULE_CONFIG =
       new SwerveModuleConfig(
           new SwerveModuleCAN(2, 1, 3, SWERVE_BUS),
           new Translation2d(X_OFFSET, -Y_OFFSET),
-          Rotation2d.fromRotations(-0.429688));
+          Rotation2d.fromRotations(-0.206787));
 
   /** Module configuration for the south east swerve module. */
   public static final SwerveModuleConfig SOUTH_EAST_MODULE_CONFIG =
       new SwerveModuleConfig(
           new SwerveModuleCAN(5, 4, 6, SWERVE_BUS),
           new Translation2d(-X_OFFSET, -Y_OFFSET),
-          Rotation2d.fromRotations(-0.097168));
+          Rotation2d.fromRotations(-0.084717));
 
   /** Module configuration for the south west swerve module. */
   public static final SwerveModuleConfig SOUTH_WEST_MODULE_CONFIG =
       new SwerveModuleConfig(
           new SwerveModuleCAN(8, 7, 9, SWERVE_BUS),
           new Translation2d(-X_OFFSET, Y_OFFSET),
-          Rotation2d.fromRotations(-0.309814));
+          Rotation2d.fromRotations(-0.442383));
 
   /**
    * Calculates the maximum attainable open loop speed in meters per second.
